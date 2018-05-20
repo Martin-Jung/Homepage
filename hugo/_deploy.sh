1ffe482ec7f83fe57b5cbc2d28c64b2adf7b95b7
Martin-Jung <martinjung@zoho.com>
2018-05-20 15:15:56 +0100

    New deploy script

--- hugo/_deploy.sh
+++ hugo/_deploy.sh
@@ -2,15 +2,10 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "sources" ] && exit 0
-
-git config --global user.email "s.e.abbott12@gmail.com"
-git config --global user.name "Sam Abbott"
-
-git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git blog-output
-cd blog-output
+rm -r public/
+hugo
+cd  public
 cp -r ../public/* ./
 git add --all *
-git commit -m "Update the blog" || true
+git commit -m "Updating..." || true
 git push -q origin master
